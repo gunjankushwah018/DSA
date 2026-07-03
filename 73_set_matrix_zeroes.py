47b8ee67d9def7b54d4e3bfb8c9df7cc1599e651
Gunjan Kushwah <gunjankushwah1717@gmail.com>
2026-07-03 10:05:01 +0530

    Update notes for LeetCode 73

--- 73_set_matrix_zeroes.py
+++ 73_set_matrix_zeroes.py
@@ -1,3 +1,14 @@
+# LeetCode 73. Set Matrix Zeroes
+# Trick used:
+#     1. Set markers in the first row and first column.
+#     2. Use a variable (col0) to track if the first column should be zeroed out.
+#     3. Update the inner matrix based on the markers.
+#     4. Fill the first row and first column based on the markers and col0 variable.
+#      
+#     Time Complexity: O(m*n) 
+#     space Complexity: O(1) (in-place modification)
+
+
 class Solution(object):
     def setZeroes(self, matrix):
         col0=1
