c658e43c9bc5795f7bfabde9a1ea8f867b0d2f80
Gunjan Kushwah <gunjankushwah1717@gmail.com>
2026-09-24 10:37:57 +0530

    Add Linked list cycle solution

---
+++ Linked List/141_Linked_list_cycle.py
@@ -0,0 +1,23 @@
+# Approach: Floyd's Cycle Detection Algorithm slow and fast pointer
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
+        
